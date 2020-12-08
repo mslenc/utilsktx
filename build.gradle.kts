2609ab4d491064490acddffdfdfede7608c8f203
Mitja Slenc <mslenc@gmail.com>
2020-12-08 16:15:17 +0100

    v1.1.0 - upgraded to Kotlin 1.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,18 +5,18 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.70")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.20")
     }
 }
 
 plugins {
     id("java-library")
-    id("org.jetbrains.kotlin.jvm") version("1.3.70")
+    id("org.jetbrains.kotlin.jvm") version("1.4.20")
     id("maven-publish")
 }
 
 group = "com.github.mslenc"
-version = "1.0.0"
+version = "1.1.0"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
@@ -32,13 +32,13 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.3.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.4.2")
 
     api("org.slf4j:slf4j-api:1.7.30")
 
     testImplementation("junit:junit:4.12")
-    testImplementation("ch.qos.logback:logback-classic:1.0.13")
+    testImplementation("ch.qos.logback:logback-classic:1.2.3")
     testImplementation(kotlin("test-junit"))
 }
 
