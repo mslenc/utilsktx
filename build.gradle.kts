52a624d3bd0f04334adc7dbcbd574b9ec696279a
Mitja Slenc <mslenc@gmail.com>
2022-08-09 23:42:35 +0200

    v1.3.0 - updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,18 +5,18 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
     }
 }
 
 plugins {
     id("java-library")
-    id("org.jetbrains.kotlin.jvm") version("1.4.32")
+    id("org.jetbrains.kotlin.jvm") version("1.6.21")
     id("maven-publish")
 }
 
 group = "com.github.mslenc"
-version = "1.2.1"
+version = "1.3.0"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
@@ -32,13 +32,13 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.4.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.6.4")
 
-    api("org.slf4j:slf4j-api:1.7.30")
+    api("org.slf4j:slf4j-api:1.7.36")
 
-    testImplementation("junit:junit:4.12")
-    testImplementation("ch.qos.logback:logback-classic:1.2.3")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("ch.qos.logback:logback-classic:1.2.11")
     testImplementation(kotlin("test-junit"))
 }
 
