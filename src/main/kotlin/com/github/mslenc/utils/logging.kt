7461637b28621f396d3cea09d3e7bf8ec252ff04
Mitja Slenc <mslenc@gmail.com>
2022-12-12 18:10:11 +0100

    v1.4.0 - upgraded kotlin etc

--- src/main/kotlin/com/github/mslenc/utils/logging.kt
+++ src/main/kotlin/com/github/mslenc/utils/logging.kt
@@ -4,53 +4,52 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import kotlin.reflect.KClass
 
-@Suppress("NOTHING_TO_INLINE")
-inline fun (() -> Any?).toStringSafe(): String {
+inline fun safeToString(producer: () -> Any?): String {
     return try {
-        invoke().toString()
+        producer().toString()
     } catch (e: Exception) {
         "Log message invocation failed: $e"
     }
 }
 
 inline fun Logger.trace(msg: () -> Any?) {
-    if (isTraceEnabled) trace(msg.toStringSafe())
+    if (isTraceEnabled) trace(safeToString(msg))
 }
 
 inline fun Logger.debug(msg: () -> Any?) {
-    if (isDebugEnabled) debug(msg.toStringSafe())
+    if (isDebugEnabled) debug(safeToString(msg))
 }
 
 inline fun Logger.info(msg: () -> Any?) {
-    if (isInfoEnabled) info(msg.toStringSafe())
+    if (isInfoEnabled) info(safeToString(msg))
 }
 
 inline fun Logger.warn(msg: () -> Any?) {
-    if (isWarnEnabled) warn(msg.toStringSafe())
+    if (isWarnEnabled) warn(safeToString(msg))
 }
 
 inline fun Logger.error(msg: () -> Any?) {
-    if (isErrorEnabled) error(msg.toStringSafe())
+    if (isErrorEnabled) error(safeToString(msg))
 }
 
 inline fun Logger.trace(t: Throwable?, msg: () -> Any?) {
-    if (isTraceEnabled) trace(msg.toStringSafe(), t)
+    if (isTraceEnabled) trace(safeToString(msg), t)
 }
 
 inline fun Logger.debug(t: Throwable?, msg: () -> Any?) {
-    if (isDebugEnabled) debug(msg.toStringSafe(), t)
+    if (isDebugEnabled) debug(safeToString(msg), t)
 }
 
 inline fun Logger.info(t: Throwable?, msg: () -> Any?) {
-    if (isInfoEnabled) info(msg.toStringSafe(), t)
+    if (isInfoEnabled) info(safeToString(msg), t)
 }
 
 inline fun Logger.warn(t: Throwable?, msg: () -> Any?) {
-    if (isWarnEnabled) warn(msg.toStringSafe(), t)
+    if (isWarnEnabled) warn(safeToString(msg), t)
 }
 
 inline fun Logger.error(t: Throwable?, msg: () -> Any?) {
-    if (isErrorEnabled) error(msg.toStringSafe(), t)
+    if (isErrorEnabled) error(safeToString(msg), t)
 }
 
 inline fun <reified T> getLogger(): Logger {
